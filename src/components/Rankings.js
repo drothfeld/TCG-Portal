57ae30323a2edc9ee473c7c16338a6c955f04785
Dylan Rothfeld <drothfel@gmail.com>
2018-11-21 13:06:48 -0800

    completed basic rankings page that is static to fire emblem cipher

--- src/components/Rankings.js
+++ src/components/Rankings.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import withAuthorization from './withAuthorization';
 import { db } from '../firebase';
+import './Rankings.css';
 
 /* Wrapped in higher order component wtih defined authorization for this component. */
 class RankingsPage extends Component {
@@ -24,7 +25,9 @@ class RankingsPage extends Component {
 
     return (
       <div>
-        <h1>USER RANKINGS</h1>
+        <div className="rankings-page-title"><h1>USER RANKINGS</h1></div>
+        <hr/>
+        <h2>FIRE EMBLEM CIPHER</h2>
 
         { !!users && <UserList users = {users}/> }
       </div>
@@ -36,10 +39,22 @@ class RankingsPage extends Component {
    must be mapped over the keys in order to display them. */
 const UserList = ({ users }) =>
   <div>
+    <div className = "rankings-row rankings-header"><b>
+      <span className = "rankings-ranking">RANK</span>
+      <span className = "rankings-user">USER</span>
+      <span className = "rankings-wins">WINS</span>
+      <span className = "rankings-losses">LOSSES</span>
+      <span className = "rankings-total">TOTAL GAMES</span>
+      <span className = "rankings-winrate">WINRATE</span></b>
+    </div>
     { Object.keys(users).map(key =>
-      <div key = { key }>
-        { users[key].username }
-        { users[key].playerStats.fireEmblemCipher.overallWinRate }
+      <div className = {"rankings-row " + ( (((key + 1) % 2) === 1) ? 'rankings-row-odd' : 'rankings-row-even')} key = { key }>
+        <span className = "rankings-ranking"><b>{ (users[key].playerStats.fireEmblemCipher.totalWins * users[key].playerStats.fireEmblemCipher.overallWinRate * 100).toFixed(0) }</b></span>
+        <span className = "rankings-user">{ users[key].username }</span>
+        <span className = "rankings-wins">{ users[key].playerStats.fireEmblemCipher.totalWins }</span>
+        <span className = "rankings-losses">{ users[key].playerStats.fireEmblemCipher.totalLosses }</span>
+        <span className = "rankings-total">{ users[key].playerStats.fireEmblemCipher.totalGames }</span>
+        <span className = "rankings-winrate">{ users[key].playerStats.fireEmblemCipher.overallWinRate.toFixed(3) * 100 }%</span>
       </div>
     )}
   </div>
