bb3a7ffc08a1c4b1f2de8c79b5c7a43f78dc50c3
Dylan Rothfeld <drothfel@gmail.com>
2018-11-27 16:23:32 -0800

    added apis for game statistics

--- src/firebase/db.js
+++ src/firebase/db.js
@@ -84,3 +84,86 @@ export const setUserGameStatsYUGIOH = (id, totalGames, totalWins, totalLosses, o
 // Get all of the game stats for a user
 export const getUserGameStats = (id) =>
   db.ref(`users/${id}/playerStats`).once('value');
+
+// Updates the global game statistics
+export const updateGlobalGameStats = (totalGamesPlayed) =>
+  db.ref(`game-statistics`).set({
+    totalGamesPlayed,
+  });
+
+// Updates the Fire Emblem Cipher game statistics
+export const updateGeneralCIPHERGameStats = (totalGamesPlayed, mostPopularDeckName, mostPopularDeckGameCount, mostVictoriousDeckName, mostVictoriousDeckWinCount, mostVictoriousDeckLossCount, mostVictoriousDeckWinRate) =>
+  db.ref(`game-statistics/fireEmblemCipher`).set({
+    totalGamesPlayed,
+    mostPopularDeckName,
+    mostPopularDeckGameCount,
+    mostVictoriousDeckName,
+    mostVictoriousDeckWinCount,
+    mostVictoriousDeckLossCount,
+    mostVictoriousDeckWinRate,
+  });
+
+// Updates the Fire Emblem Cipher game statistics
+export const updateCIPHERGameStats = (color, totalGamesPlayed, totalWins, totalLosses, mostPopularDeckName, mostPopularDeckGameCount, mostVictoriousDeckName, mostVictoriousDeckWinCount, mostVictoriousDeckLossCount, mostVictoriousDeckWinRate,
+  winsAgainstRed, lossesAgainstRed, winRateAgainstRed,
+  winsAgainstBlue, lossesAgainstBlue, winRateAgainstBlue,
+  winsAgainstWhite, lossesAgainstWhite, winRateAgainstWhite,
+  winsAgainstBlack, lossesAgainstBlack, winRateAgainstBlack,
+  winsAgainstGreen, lossesAgainstGreen, winRateAgainstGreen,
+  winsAgainstPurple, lossesAgainstPurple, winRateAgainstPurple,
+  winsAgainstYellow, lossesAgainstYellow, winRateAgainstYellow,
+  winsAgainstColorless, lossesAgainstColorless, winRateAgainstColorless,
+  ) =>
+  db.ref(`game-statistics/fireEmblemCipher/${color}`).set({
+    totalGamesPlayed,
+    totalWins,
+    totalLosses,
+    mostPopularDeckName,
+    mostPopularDeckGameCount,
+    mostVictoriousDeckName,
+    mostVictoriousDeckWinCount,
+    mostVictoriousDeckLossCount,
+    mostVictoriousDeckWinRate,
+    winsAgainstRed, lossesAgainstRed, winRateAgainstRed,
+    winsAgainstBlue, lossesAgainstBlue, winRateAgainstBlue,
+    winsAgainstWhite, lossesAgainstWhite, winRateAgainstWhite,
+    winsAgainstBlack, lossesAgainstBlack, winRateAgainstBlack,
+    winsAgainstGreen, lossesAgainstGreen, winRateAgainstGreen,
+    winsAgainstPurple, lossesAgainstPurple, winRateAgainstPurple,
+    winsAgainstYellow, lossesAgainstYellow, winRateAgainstYellow,
+    winsAgainstColorless, lossesAgainstColorless, winRateAgainstColorless,
+  });
+
+// Updates the Magic The Gathering game statistics
+export const updateGeneralMAGICGameStats = (totalGamesPlayed) =>
+  db.ref(`game-statistics/magicTheGathering`).set({
+    totalGamesPlayed,
+  });
+
+// Updates the Pokémon game statistics
+export const updateGeneralPOKEMONGameStats = (totalGamesPlayed) =>
+  db.ref(`game-statistics/pokemon`).set({
+    totalGamesPlayed,
+  });
+
+// Updates the Yu-Gi-Oh game statistics
+export const updateGeneralYUGIOHGameStats = (totalGamesPlayed) =>
+  db.ref(`game-statistics/yugioh`).set({
+    totalGamesPlayed,
+  });
+
+// Get all the game stats for Fire Emblem Cipher
+export const getCIPHERGameStats = () =>
+  db.ref(`game-statistics/fireEmblemCipher`).once('value');
+
+// Get all the game stats for Magic The Gathering
+export const getMAGICGameStats = () =>
+  db.ref(`game-statistics/magicTheGathering`).once('value');
+
+// Get all the game stats for Pokémon
+export const getPOKEMONGameStats = () =>
+  db.ref(`game-statistics/pokemon`).once('value');
+
+// Get all the game stats for Yu-Gi-Oh
+export const getYUGIOHGameStats = () =>
+  db.ref(`game-statistics/yugioh`).once('value');
