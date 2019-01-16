c0193c216de347dbc8791276d07ad7a8db5d02ef
Dylan Rothfeld <drothfel@gmail.com>
2019-01-16 13:49:52 -0800

    setup for calling api to update cipher game statistics

--- src/components/tabs/stats/CipherStats.js
+++ src/components/tabs/stats/CipherStats.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 
+import withAuthorization from '../../auth/withAuthorization';
+import { db } from '../../../firebase';
+import * as cardGames from '../../../constants/cardGames';
+import './Stats.css';
 
 class CipherStatsPage extends Component {
   // constructor(props) {
@@ -8,9 +12,50 @@ class CipherStatsPage extends Component {
 
   render() {
     return (
-      <h1>FIRE EMBLEM CIPHER STATS</h1>
+      <div>
+        <h1>FIRE EMBLEM CIPHER STATS</h1>
+        <CipherGameStatistics/>
+      </div>
     );
   }
 }
 
-export default CipherStatsPage;
+class CipherGameStatistics extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      games: null
+    };
+  }
+
+  componentDidMount() {
+    db.onceGetRecordedGames().then(snapshot =>
+      this.setState({ games: snapshot.val() })
+    );
+  }
+
+  updateCipherGameStats(games) {
+    for (var key in games) {
+      var game = games[key];
+      if (game.cardGame === cardGames.FE_CIPHER) {
+        // console.log(game);
+      }
+    }
+  }
+
+  render() {
+    const { games } = this.state;
+    this.updateCipherGameStats(games);
+
+    return (
+      <div>
+      </div>
+    )
+  }
+
+}
+
+const authCondition = (authUser) => !!authUser;
+
+export default withAuthorization(authCondition)(CipherStatsPage);
